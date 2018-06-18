fd3ffc0da7d6a7153300e40e37a69abaad29bc8f
Enmanuel paredes <EnmanuelParedesr@hotmail.com>
2018-06-17 21:29:29 -0400

    Original

---
+++ Video_Kata/DLinkedListUnitTest/UnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Video_Kata;
+
+namespace DLinkedListUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AddValueToTheList()
+        {
+            DLinkedList DL = new  DLinkedList();
+
+            DL.Add("Amigo");
+            DL.Add("Hermano");
+
+            Assert.AreEqual("Hermano", DL.Tail.Value);
+
+        }
+
+        [TestMethod]
+        public void FindNodeInTheList()
+        {
+            DLinkedList DL = new DLinkedList();
+
+            DL.Add("Amigo");
+            DL.Add("Hermano");
+            
+            Assert.AreEqual("Hermano", DL.FindNode("Hermano"));
+        }
+
+        [TestMethod]
+        public void DeleteANodeInAListWith1Node()
+        {
+            DLinkedList DL = new DLinkedList();
+
+            DL.Add("Hermano");
+
+            DL.Delete("Hermano");
+
+            Assert.AreNotEqual("Hermano", DL.FindNode("Hermano"));
+        }
+
+
+        [TestMethod]
+        public void GeneralDelete()
+        {
+            DLinkedList DL = new DLinkedList();
+
+            DL.Add("Hermano");
+            DL.Add("Amigo");
+
+            DL.Delete("Hermano");
+
+            Assert.AreNotEqual("Hermano", DL.FindNode("Hermano"));
+        }
+
+        [TestMethod]
+        public void ShowValues()
+        {
+            DLinkedList DL = new DLinkedList();
+
+            DL.Add("Hermano");
+            DL.Add("Amigo");
+
+            Assert.AreEqual("(Hermano, Amigo)", DL.ToString());
+        }
+
+        [TestMethod]
+        public void PreprendTwoListWhenSecondIsEmpty()
+        {
+            DLinkedList DL = new DLinkedList();
+            DLinkedList DL2 = new DLinkedList();
+
+            DL.Add("Hermano");
+            DL.Add("Amigo");
+
+
+            DL.Preprend(DL2);
+
+            Assert.AreEqual("(Hermano, Amigo)", DL.ToString());
+        }
+
+    
+
+        [TestMethod]
+        public void PreprendTwoListWhenFirstIsEmpty()
+        {
+            DLinkedList DL = new DLinkedList();
+            DLinkedList DL2 = new DLinkedList();
+
+           
+            DL2.Add("Jesus");
+
+            DL2.Preprend(DL);
+
+            Assert.AreEqual("(Jesus)", DL2.ToString());
+        }
+
+        [TestMethod]
+        public void PreprendTwoList()
+        {
+            DLinkedList DL = new DLinkedList();
+            DLinkedList DL2 = new DLinkedList();
+
+            DL.Add("Hermano");
+            DL.Add("Amigo");
+            DL2.Add("Jesus");
+
+            DL.Preprend(DL2);
+
+            Assert.AreEqual("(Jesus, Hermano, Amigo)", DL.ToString());
+        }
+
+
+    }
+}
---
+++ Video_Kata/Video_Kata/DLinkedList.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Video_Kata
+{
+    public class DLinkedList
+    {
+
+        public class DLinkNode
+        {
+            public String Value;
+            public DLinkNode next, prev;
+
+            public DLinkNode(String Value)
+            {
+               this.Value = Value;
+            }
+
+            
+        }
+
+        public DLinkNode Head,Tail;
+        public int Size { get; private set; }
+
+        public void Add(String value)
+        {
+            DLinkNode NewNode = new DLinkNode(value);
+
+            if(Size == 0)
+            {
+                Head = Tail = NewNode;
+                Size++;
+                return;
+            }
+
+            Tail.next = NewNode;
+            NewNode.prev = Tail;
+            Tail = NewNode;
+            Size++;
+
+            return;
+        }
+
+        public String FindNode(String value)
+        {
+            DLinkNode cur = Head;
+
+            for (int i = 0; i < Size; i++)
+            {
+                if (cur.Value == value)
+                {
+                    return cur.Value;
+                }
+                else
+                {
+                    cur = cur.next;
+                }
+            }
+           
+            return null;   
+        }
+
+        public void Delete(String Value)
+        {
+            DLinkNode cur = Head;
+
+            for (int i = 0; i < Size; i++)
+            {
+                if (cur.Value == Value)
+                {
+                    if (i == 0)
+                    {
+                        Head = Head.next;
+
+                        if (Size != 1)
+                            cur.next.prev = null;
+                        else
+                            Tail = null;
+
+                        Size--;
+                        return;
+                    }
+
+                    if (i == Size - 1)
+                    {
+                        Tail = Tail.prev;
+                        cur.prev.next = null;
+                        Size--;
+                        return;
+                    }
+
+                    cur.prev.next = cur.next;
+                    cur.next.prev = cur.prev;
+                    Size--;
+                    return;
+                }
+                else
+                {
+                    cur = cur.next;
+                }
+            }
+
+        }
+
+
+        override public String ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("(");
+            DLinkNode cur = Head;
+            while (cur != null)
+            {
+                if (cur != Head)
+                    sb.Append(", ");
+                sb.Append(cur.Value);
+                cur = cur.next;
+            }
+            sb.Append(")");
+            return sb.ToString();
+        }
+
+        public void Preprend(DLinkedList L)
+        {
+            if (L.Size == 0)
+                return;
+
+           if(this.Size == 0)
+            {
+                this.Head = L.Head;
+                this.Tail = L.Tail;
+                this.Size = L.Size;
+
+                L.Head = L.Tail = null;
+                L.Size = 0;
+
+                return;
+            }
+
+            L.Tail.next = Head;
+            this.Head.prev = Tail;
+            this.Head = L.Head;
+            this.Size += L.Size;
+
+            L.Head = L.Tail = null;
+            L.Size = 0;
+
+        }
+
+
+    }
+}
